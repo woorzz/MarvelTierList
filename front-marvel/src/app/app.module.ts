30726275d44cce75bf690c46c2a09fffcf906c20
PaulDebril <debril.paul@gmail.com>
2024-02-01 16:49:02 +0100

    Update

--- front-marvel/src/app/app.module.ts
+++ front-marvel/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    MainmenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-marvel/src/app/mainmenu/mainmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainmenu',
+  templateUrl: './mainmenu.component.html',
+  styleUrl: './mainmenu.component.scss'
+})
+export class MainmenuComponent {
+
+}
