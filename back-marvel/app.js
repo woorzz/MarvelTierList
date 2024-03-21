70d7d7ab837f927f6e142c1fd92bfa3c18e8f88d
judekabeya <judek9392@gmail.com>
2024-03-21 15:22:26 +0100

    feature : search character

--- back-marvel/app.js
+++ back-marvel/app.js
@@ -32,6 +32,34 @@ app.get('/marvel/spider', async (req, res) => {
   }
 });
 
+
+app.get('/marvel/search/:characterName', async (req, res) => {
+  try {
+    const publicKey = process.env.PUBLIC_KEY;
+    const privateKey = process.env.PRIVATE_KEY;
+    const timestamp = new Date().getTime();
+    const hash = require('crypto').createHash('md5').update(timestamp + privateKey + publicKey).digest('hex');
+    
+    const characterName = req.params.characterName;
+
+    const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}&limit=100&nameStartsWith=${characterName}`);
+
+    const characters = response.data.data.results.map(character => ({
+      id: character.id,
+      name: character.name,
+      thumbnail: character.thumbnail ? `${character.thumbnail.path}/portrait_medium.${character.thumbnail.extension}` : null
+    })).filter(char => {
+      return char.thumbnail && !char.thumbnail.includes('image_not_available');
+    });
+
+    res.json(characters);
+  } catch (error) {
+    console.error('Erreur :', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération des personnages' });
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Serveur backend démarré sur le port ${PORT}`);
 });
